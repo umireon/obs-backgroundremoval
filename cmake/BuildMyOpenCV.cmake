80ae2bcefb7f712119c428f688fcb8bccb3232aa
Kaito Udagawa <umireon@gmail.com>
2023-01-24 20:06:40 +0900

    Update BuildMyOpenCV.cmake

--- cmake/BuildMyOpenCV.cmake
+++ cmake/BuildMyOpenCV.cmake
@@ -31,7 +31,6 @@ endif()
 ExternalProject_Add(
   OpenCV_Build
   URL https://github.com/umireon/opencv/archive/refs/tags/4.7.1.tar.gz
-  BUILD_COMMAND cmake --build <BINARY_DIR>
   BUILD_BYPRODUCTS
     <INSTALL_DIR>/${OpenCV_LIB_PATH}/${CMAKE_STATIC_LIBRARY_PREFIX}opencv_core${OpenCV_LIB_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}
     <INSTALL_DIR>/${OpenCV_LIB_PATH}/${CMAKE_STATIC_LIBRARY_PREFIX}opencv_features2d${OpenCV_LIB_SUFFIX}${CMAKE_STATIC_LIBRARY_SUFFIX}
