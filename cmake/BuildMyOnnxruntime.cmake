82e6679131d419de92647fa0949707cd924a8abb
Kaito Udagawa <umireon@gmail.com>
2023-01-25 05:20:15 +0900

    fix

--- cmake/BuildMyOnnxruntime.cmake
+++ cmake/BuildMyOnnxruntime.cmake
@@ -31,6 +31,7 @@ ExternalProject_Add(
   BUILD_COMMAND
     ${PYTHON} <SOURCE_DIR>/tools/ci_build/build.py --build_dir <BINARY_DIR>
     --config ${CMAKE_BUILD_TYPE} --parallel --skip_tests --skip_submodule_sync
+    --apple_deploy_target=${CMAKE_OSX_DEPLOYMENT_TARGET}
   BUILD_BYPRODUCTS
     <INSTALL_DIR>/lib/${CMAKE_STATIC_LIBRARY_PREFIX}onnxruntime_session${CMAKE_STATIC_LIBRARY_SUFFIX}
     <INSTALL_DIR>/lib/${CMAKE_STATIC_LIBRARY_PREFIX}onnxruntime_framework${CMAKE_STATIC_LIBRARY_SUFFIX}
