5689a1761cb6ba83f28972da28204773f1070284
Kaito Udagawa <umireon@gmail.com>
2023-01-27 23:29:13 +0900

    Update BuildMyOnnxruntime.cmake

--- cmake/BuildMyOnnxruntime.cmake
+++ cmake/BuildMyOnnxruntime.cmake
@@ -143,5 +143,5 @@ if(OS_WINDOWS)
     Onnxruntime::DirectML
     PROPERTIES IMPORTED_IMPLIB ${INSTALL_DIR}/lib/DirectML.lib
   )
-  target_link_libraries(Onnxruntime INTERFACE Onnxruntime::DirectML d3d12.lib dxgi.lib)
+  target_link_libraries(Onnxruntime INTERFACE Onnxruntime::DirectML d3d12.lib dxgi.lib dxguid.lib)
 endif()
