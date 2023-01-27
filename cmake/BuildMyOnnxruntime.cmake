0faba648c925b3a43d0b8b966c352d460011a290
Kaito Udagawa <umireon@gmail.com>
2023-01-27 21:53:09 +0900

    Update BuildMyOnnxruntime.cmake

--- cmake/BuildMyOnnxruntime.cmake
+++ cmake/BuildMyOnnxruntime.cmake
@@ -13,9 +13,6 @@ endif()
 
 if(OS_WINDOWS)
   set(Onnxruntime_PLATFORM_OPTIONS --cmake_generator ${CMAKE_GENERATOR})
-  set(Onnxruntime_NSYNC_BYPRODUCT "")
-  set(Onnxruntime_NSYNC_INSTALL "")
-  set(Onnxruntime_PROTOBUF_PREFIX lib)
   set(Onnxruntime_CMAKE_BINARY_DIR <BINARY_DIR>/${CMAKE_BUILD_TYPE})
 
   set(Onnxruntime_BUILD_BYPRODUCTS <INSTALL_DIR>/lib/onnxruntime.lib
@@ -24,14 +21,7 @@ elseif()
   set(Onnxruntime_PLATFORM_OPTIONS
       --apple_deploy_target ${CMAKE_OSX_DEPLOYMENT_TARGET} --osx_arch
       ${CMAKE_OSX_ARCHITECTURES_})
-  set(Onnxruntime_NSYNC_BYPRODUCT
-      <INSTALL_DIR>/lib/${CMAKE_STATIC_LIBRARY_PREFIX}nsync_cpp${CMAKE_STATIC_LIBRARY_SUFFIX}
-  )
-  set(Onnxruntime_NSYNC_BINARY
-      <BINARY_DIR>/${Onnxruntime_LIB_PREFIX}/external/nsync/${Onnxruntime_LIB_PREFIX}/${CMAKE_STATIC_LIBRARY_PREFIX}nsync_cpp${CMAKE_STATIC_LIBRARY_SUFFIX}
-  )
-  set(Onnxruntime_PROTOBUF_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
-  set(Onnxruntime_CMAKE_BINARY_DIR <BINARY_DIR>)
+  set(Onnxruntime_CMAKE_BINARY_DIR <BINARY_DIR>/${CMAKE_BUILD_TYPE})
 
   set(Onnxruntime_BUILD_BYPRODUCTS
       <INSTALL_DIR>/lib/${CMAKE_SHARED_LIBRARY_PREFIX}onnxruntime.${Onnxruntime_VERSION}${CMAKE_SHARED_LIBRARY_SUFFIX}
@@ -51,10 +41,8 @@ ExternalProject_Add(
     ${Onnxruntime_PLATFORM_OPTIONS} --build_shared_lib --cmake_extra_defines
     onnxruntime_BUILD_UNIT_TESTS=OFF
   BUILD_BYPRODUCTS ${Onnxruntime_BUILD_BYPRODUCTS}
-  INSTALL_COMMAND
-    ${CMAKE_COMMAND} --install
-    ${Onnxruntime_CMAKE_BINARY_DIR}/${CMAKE_BUILD_TYPE} --config
-    ${CMAKE_BUILD_TYPE} --prefix <INSTALL_DIR>)
+  INSTALL_COMMAND ${CMAKE_COMMAND} --install ${Onnxruntime_CMAKE_BINARY_DIR}
+                  --config ${CMAKE_BUILD_TYPE} --prefix <INSTALL_DIR>)
 
 ExternalProject_Get_Property(Ort INSTALL_DIR)
 
