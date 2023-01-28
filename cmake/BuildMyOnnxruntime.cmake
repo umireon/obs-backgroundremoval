d586e102e037564d0050eaf7d485f01c2b53735d
Kaito Udagawa <umireon@gmail.com>
2023-01-28 14:05:25 +0900

    Update BuildMyOnnxruntime.cmake

--- cmake/BuildMyOnnxruntime.cmake
+++ cmake/BuildMyOnnxruntime.cmake
@@ -35,7 +35,7 @@ elseif(OS_MACOS)
       CMAKE_C_COMPILER_LAUNCHER=ccache
       --cmake_extra_defines
       CMAKE_CXX_COMPILER_LAUNCHER=ccache)
-  set(Onnxruntime_NSYNC_BYPRODUCT
+  set(Onnxruntime_PLATFORM_BYPRODUCT
       <INSTALL_DIR>/lib/${CMAKE_STATIC_LIBRARY_PREFIX}nsync_cpp${CMAKE_STATIC_LIBRARY_SUFFIX}
   )
   set(Onnxruntime_PLATFORM_INSTALL_FILES
