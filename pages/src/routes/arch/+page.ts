cf63d88be059e3b0e05f4f71836ab23445f561bc
Kaito Udagawa <umireon@gmail.com>
2023-10-05 01:27:59 +0900

    Add page.server.ts

--- pages/src/routes/arch/+page.ts
+++ pages/src/routes/arch/+page.ts
@@ -1,7 +1,5 @@
 import { dev } from '$app/environment';
 
-import { getLatestReleaseMetadata } from '../../lib/github-release'
-
 // we don't need any JS on this page, though we'll load
 // it in dev so that we get hot module replacement
 export const csr = dev;
